99c90e54bed951731f4a1570c786ec703c7c1384
Yimura <andreas.maerten@scarlet.be>
2021-03-23 12:54:16 +0100

    wip(GUI): Basic communication POC

--- client/gui.py
+++ client/gui.py
@@ -1,19 +1,23 @@
 import threading
 
 class GUI(threading.Thread):
-    def __init__(self):
+    def __init__(self, backend):
         threading.Thread.__init__(self, daemon=True)
 
         self._running = True
+
+        self.backend = backend
 
     @property
     def running(self):
         return self._running
 
     def run(self):
-        
         while self._running:
-            pass
+            event = input("EVENT > ")
+            data = input("DATA > ")
+
+            self.backend.send_event(event, data)
 
     def terminate(self):
         self._running = False
