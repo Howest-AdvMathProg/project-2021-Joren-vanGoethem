c081305cb2ab4062b43c4949c3e79a6b01763c5e
Yimura <andreas.maerten@scarlet.be>
2021-03-23 12:53:13 +0100

    feat(Client): Added EventHandler

---
+++ client/handler.py
@@ -0,0 +1,35 @@
+import threading
+# pylint: disable=no-name-in-module
+from util.logger import Log
+
+class EventHandler(threading.Thread):
+    def __init__(self, backend):
+        threading.Thread.__init__(self, daemon=True)
+
+        self._running = True
+
+        self.backend = backend
+
+    @property
+    def running(self):
+        return self._running
+
+    def run(self):
+        while self._running:
+            msg = self.backend.queue.get()
+
+            handler = getattr(self, msg["event"].lower(), False)
+            if not handler:
+                Log.warning("EVENT_HANDLER", "No handler exists for event "+ msg["event"])
+            else:
+                handler(msg["data"])
+
+            Log.verbose("EVENT_HANDLER", f"[{msg['event']}]: {msg['data']}")
+
+            self.backend.queue.task_done()
+
+    def message(self, data):
+        print(data)
+
+    def terminate(self):
+        self._running = False
