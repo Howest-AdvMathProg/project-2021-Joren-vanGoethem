ce257d1cb15449864860d035dfc3e4534ad807ef
Joren-vanGoethem <jorenvangoethem@hotmail.com>
2021-04-17 21:26:17 +0200

    ui change

--- client/gui/ui/mainwindowui.py
+++ client/gui/ui/mainwindowui.py
@@ -21,46 +21,69 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(490, 349)
+        MainWindow.resize(703, 414)
         sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
         MainWindow.setSizePolicy(sizePolicy)
-        self.actionUser = QAction(MainWindow)
-        self.actionUser.setObjectName(u"actionUser")
-        self.actionModerator = QAction(MainWindow)
-        self.actionModerator.setObjectName(u"actionModerator")
         self.actiontest = QAction(MainWindow)
         self.actiontest.setObjectName(u"actiontest")
+        self.actionQuery_Log = QAction(MainWindow)
+        self.actionQuery_Log.setObjectName(u"actionQuery_Log")
+        self.actionServer_Log = QAction(MainWindow)
+        self.actionServer_Log.setObjectName(u"actionServer_Log")
+        self.actionActive_Users = QAction(MainWindow)
+        self.actionActive_Users.setObjectName(u"actionActive_Users")
+        self.actionAbout = QAction(MainWindow)
+        self.actionAbout.setObjectName(u"actionAbout")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.centralwidget.setEnabled(True)
-        self.verticalLayout = QVBoxLayout(self.centralwidget)
-        self.verticalLayout.setObjectName(u"verticalLayout")
-        self.textEdit = QTextEdit(self.centralwidget)
-        self.textEdit.setObjectName(u"textEdit")
+        self.gridLayout = QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.gridLayout.setSizeConstraint(QLayout.SetDefaultConstraint)
+        self.Resultlabel = QLabel(self.centralwidget)
+        self.Resultlabel.setObjectName(u"Resultlabel")
 
-        self.verticalLayout.addWidget(self.textEdit)
+        self.gridLayout.addWidget(self.Resultlabel, 0, 0, 1, 1)
 
+        self.commandfield = QPlainTextEdit(self.centralwidget)
+        self.commandfield.setObjectName(u"commandfield")
+
+        self.gridLayout.addWidget(self.commandfield, 1, 1, 1, 1)
+
+        self.commandlabel = QLabel(self.centralwidget)
+        self.commandlabel.setObjectName(u"commandlabel")
+
+        self.gridLayout.addWidget(self.commandlabel, 0, 1, 1, 1)
+
+        self.Searchbutton = QPushButton(self.centralwidget)
+        self.Searchbutton.setObjectName(u"Searchbutton")
+
+        self.gridLayout.addWidget(self.Searchbutton, 2, 1, 1, 1)
+
+        self.ResultTable = QTableView(self.centralwidget)
+        self.ResultTable.setObjectName(u"ResultTable")
+        self.ResultTable.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.ResultTable.setAlternatingRowColors(True)
+        self.ResultTable.setSortingEnabled(True)
+        self.ResultTable.verticalHeader().setProperty("showSortIndicator", True)
+
+        self.gridLayout.addWidget(self.ResultTable, 1, 0, 2, 1)
+
+        self.gridLayout.setColumnStretch(0, 1)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 490, 30))
-        self.menuLogin = QMenu(self.menubar)
-        self.menuLogin.setObjectName(u"menuLogin")
-        self.menuLog = QMenu(self.menubar)
-        self.menuLog.setObjectName(u"menuLog")
+        self.menubar.setGeometry(QRect(0, 0, 703, 30))
+        self.menuExtra = QMenu(self.menubar)
+        self.menuExtra.setObjectName(u"menuExtra")
         MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QStatusBar(MainWindow)
-        self.statusbar.setObjectName(u"statusbar")
-        MainWindow.setStatusBar(self.statusbar)
 
-        self.menubar.addAction(self.menuLogin.menuAction())
-        self.menubar.addAction(self.menuLog.menuAction())
-        self.menuLogin.addAction(self.actionUser)
-        self.menuLogin.addAction(self.actionModerator)
-        self.menuLog.addAction(self.actiontest)
+        self.menubar.addAction(self.menuExtra.menuAction())
+        self.menuExtra.addAction(self.actiontest)
+        self.menuExtra.addAction(self.actionAbout)
 
         self.retranslateUi(MainWindow)
 
@@ -69,9 +92,25 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Python Client", None))
-        self.actionUser.setText(QCoreApplication.translate("MainWindow", u"User", None))
-        self.actionModerator.setText(QCoreApplication.translate("MainWindow", u"Moderator", None))
         self.actiontest.setText(QCoreApplication.translate("MainWindow", u"test", None))
-        self.menuLogin.setTitle(QCoreApplication.translate("MainWindow", u"Login", None))
-        self.menuLog.setTitle(QCoreApplication.translate("MainWindow", u"Log", None))
+        self.actionQuery_Log.setText(QCoreApplication.translate("MainWindow", u"Query Log", None))
+        self.actionServer_Log.setText(QCoreApplication.translate("MainWindow", u"Server Log", None))
+        self.actionActive_Users.setText(QCoreApplication.translate("MainWindow", u"Active Users", None))
+        self.actionAbout.setText(QCoreApplication.translate("MainWindow", u"About", None))
+        self.Resultlabel.setText(QCoreApplication.translate("MainWindow", u"Result", None))
+        self.commandfield.setPlainText(QCoreApplication.translate("MainWindow", u"{\n"
+"  \"event\": \"SEARCH\",\n"
+"  \"data\": {\n"
+"    \"query\": {\n"
+"      \"column\": \"\",\n"
+"      \"value\": \"\"\n"
+"    },\n"
+"    \"type\": \"column\",\n"
+"    \"exact\": false\n"
+"  }\n"
+"}", None))
+        self.commandlabel.setText(QCoreApplication.translate("MainWindow", u"Search Command", None))
+        self.Searchbutton.setText(QCoreApplication.translate("MainWindow", u"Search", None))
+        self.menuExtra.setTitle(QCoreApplication.translate("MainWindow", u"Extra", None))
     # retranslateUi
+
