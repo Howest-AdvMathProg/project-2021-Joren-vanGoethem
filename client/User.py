f575e24dbcfb0a9f986d9b44148d2edcefbe8de2
Joren-vanGoethem <jorenvangoethem@hotmail.com>
2021-04-21 16:13:43 +0200

    User class

---
+++ client/User.py
@@ -0,0 +1,6 @@
+class User():
+    def __init__(self, username, nickname, email, password):
+        self.username = username
+        self.nickname = nickname
+        self.email = email
+        self.password = password
